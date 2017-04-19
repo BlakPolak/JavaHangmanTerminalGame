2b3f96750034fd1bed999ce687e14021dc9bea97
Beata <beata.k.puzio@gmail.com>
2017-04-19 13:14:23 +0200

    txt file modified and method to read file created

--- FileSupport.java
+++ FileSupport.java
@@ -1,19 +1,36 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class ReadCsv {
-    LinkedList<String[]> rows = new LinkedList<String[]>();
-    String dataRow = CSVFile.readLine();
-    // Read the number of the lines in .csv file
-    // i = row of the .csv file
-    int i = 0;
-    while ((datarow = CSVFile.readLine()) != null){
-        i++;
-        rows.addLast(dataRow.split(","));
+public class FileSupport {
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+
+        // // read KeyWestTemp.txt
+
+        // create token1
+        String token1 = "";
+
+        // for-each loop for calculating heat index of May - October
+
+        // create Scanner inFile1
+        Scanner inFile1 = new Scanner(new File("europecapitals.txt")).useDelimiter(",\\s*");
+
+        // Original answer used LinkedList, but probably preferable to use ArrayList in most cases
+        // List<String> temps = new LinkedList<String>();
+        List<String> temps = new ArrayList<String>();
+
+        // while loop
+        while (inFile1.hasNext()) {
+          // find next line
+          token1 = inFile1.next();
+          temps.add(token1);
+        }
+        inFile1.close();
+        String[] tempsArray = temps.toArray(new String[0]);
+
+        // for (String s : tempsArray) {
+        //     System.out.println(s);
+        return tempsArray;
     }
-
+  }
 }
